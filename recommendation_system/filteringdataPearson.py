d351027c80e5b4ab36854cce6e06ae679ecad4be
Eric Chen <eriolchan@gmail.com>
2015-12-30 21:37:06 +0800

    checkin for peason

--- recommendation_system/filteringdataPearson.py
+++ recommendation_system/filteringdataPearson.py
@@ -1,23 +1,52 @@
+import math
+
 users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phonenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0}, "Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phonenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0}, "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phonenix": 5.0, "Slightly Stoopid": 1.0}, "Dan": {"Blues Traveler": 3.0,
 "Broken Bells": 4.0, "Deadmau5": 4.5, "Phonenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0}, "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0}, "Jordyn": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phonenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0}, "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phonenix": 5.0,
 "Slightly Stoopid": 4.0, "The Strokes": 5.0}, "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phonenix": 4.0,
 "Slightly Stoopid": 2.5, "The Strokes": 3.0}}
 
 
-def minkowski(rating1, rating2, r):
-  """Computes the Minkowski distance. Both rating1 and rating2 are
-  dictionaries of the form
-  {'The Strokes': 3.0, 'Slightly Stoopid': 2.5 ..."""
+def manhattan(rating1, rating2):
+  """Computes the Manhattan distance. Both rating1 and rating2 are dictionaries
+  of the form {'The Strokes': 3.0, 'Slightly Stoopid': 2.5)"""
   distance = 0
-  common_ratings = False
+  total = 0
   for key in rating1:
     if key in rating2:
-      distance += pow(abs(rating1[key] - rating2[key]), r)
-      common_ratings = True
-  if common_ratings:
-    return pow(distance, 1/r)
+      distance += abs(rating1[key] - rating2[key])
+      total += 1
+  if total > 0:
+    return distance / total
   else:
     return -1 # Indicates no ratings in common
+
+
+def pearson(rating1, rating2):
+  sum_xy = 0
+  sum_x = 0
+  sum_y = 0
+  sum_x2 = 0
+  sum_y2 = 0
+  n = 0
+  for key in rating1:
+    if key in rating2:
+      n += 1
+      x = rating1[key]
+      y = rating2[key]
+      sum_xy += x * y
+      sum_x += x
+      sum_y += y
+      sum_x2 += x ** 2
+      sum_y2 += y ** 2
+  # if no ratings in common, return 0
+  if n == 0:
+    return 0
+  # now compute denominator
+  denominator = math.sqrt(sum_x2 - (sum_x ** 2) / n) * math.sqrt(sum_y2 - (sum_y ** 2) / n)
+  if denominator == 0:
+    return 0
+  else:
+    return (sum_xy - (sum_x * sum_y) / n) / denominator
 
 
 def compute_nearest_neighbor(username, users):
@@ -26,8 +55,7 @@ def compute_nearest_neighbor(username, users):
   distances = []
   for user in users:
     if user != username:
-      # distance = minkowski(users[user], users[username], 1) -> Manhattan distance
-      distance = minkowski(users[user], users[username], 2) # Euclidean distance
+      distance = manhattan(users[user], users[username])
       distances.append((distance, user))
   # sort based on distance -- closest first
   return sorted(distances)
@@ -49,4 +77,4 @@ def recommend(username, users):
 
 
 # test
-print recommend('Hailey', users)
+print pearson(users['Angelica'], users['Jordyn'])
